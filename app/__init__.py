5b2fe822b8828e3d900e8d8ce4d8cdabd48722a8
dibakash <dibakash@gmail.com>
2022-01-14 22:34:02 +0530

    configure heroku

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from tempfile import mkdtemp
 from functools import wraps
 from cs50 import SQL
 
-# from tempfile import mkdtemp
+from tempfile import mkdtemp
 
 from cs50 import SQL
 
@@ -31,7 +31,8 @@ def create_app():
         response.headers["Pragma"] = "no-cache"
         return response
 
-    # app.config["SESSION_FILE_DIR"] = mkdtemp()
+    # Configure session to use filesystem (instead of signed cookies)
+    app.config["SESSION_FILE_DIR"] = mkdtemp()
     app.config["SESSION_PERMANENT"] = False
     app.config["SESSION_TYPE"] = "filesystem"
     Session(app)
